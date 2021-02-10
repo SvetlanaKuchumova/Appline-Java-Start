d7c7dcdb814dc00c7ba24e7ebcc5ab70b809b529
Svetlana Kuchumova <svetikpodv@yandex.ru>
2021-02-10 20:53:55 +0500

    Convert binary number to decimal (Task 3)

---
+++ src/BinNumberTask3.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BinNumberTask3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input a binary number: ");
+        //Input a binary number
+        String BinNumber = scan.nextLine();
+        //Convert binary number to decimal
+        int DecNumb = Integer.parseInt(BinNumber, 2);
+        //Output a decimal number
+        System.out.print("Decimal number: ");
+        System.out.println(DecNumb);
+    }
+}
