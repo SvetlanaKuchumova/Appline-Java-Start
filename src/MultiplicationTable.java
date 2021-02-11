789979b8a869647b20862b403eab26ada45773c3
Svetlana Kuchumova <svetikpodv@yandex.ru>
2021-02-11 11:08:55 +0500

    multiplication table (task 4)

---
+++ src/MultiplicationTable.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Input a number: ");
+
+        //Input a number
+        double Number = scan.nextDouble();
+        //Counter from 1 to 10
+        int i = 1;
+
+        //Output the multiplication table
+        System.out.println(i + " x " + Number + " = " + Number*i++);
+        System.out.println(i + " x " + Number + " = " + Number*i++);
+        System.out.println(i + " x " + Number + " = " + Number*i++);
+        System.out.println(i + " x " + Number + " = " + Number*i++);
+        System.out.println(i + " x " + Number + " = " + Number*i++);
+        System.out.println(i + " x " + Number + " = " + Number*i++);
+        System.out.println(i + " x " + Number + " = " + Number*i++);
+        System.out.println(i + " x " + Number + " = " + Number*i++);
+        System.out.println(i + " x " + Number + " = " + Number*i++);
+        System.out.println(i + " x " + Number + " = " + Number*i);
+    }
+}
