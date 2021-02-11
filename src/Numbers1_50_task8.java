eef456d48541b820f1707c32a4a45f3f15078fc5
Svetlana Kuchumova <svetikpodv@yandex.ru>
2021-02-11 21:39:15 +0500

    Output Numbers from 1 to 50 (task 8)

---
+++ src/Numbers1_50_task8.java
@@ -0,0 +1,9 @@
+public class Numbers1_50_task8 {
+    public static void main(String[] args) {
+
+        //Output Numbers from 1 to 50
+        for(int i = 1; i <= 50; i++ ){
+            System.out.println(i);
+        }
+    }
+}
