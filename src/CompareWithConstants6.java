12e996efb5a0835da664ddb236fc4b6355048cd6
Svetlana Kuchumova <svetikpodv@yandex.ru>
2021-02-11 20:43:07 +0500

    Compare with constants (task 6)

---
+++ src/CompareWithConstants6.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CompareWithConstants6 {
+    public static void main(String[] args) {
+
+        //Constants
+        double X = 10.001;
+        double Y = 11.0099;
+        double Z = 1200;
+
+        System.out.print("Input a number: ");
+        Scanner scan = new Scanner(System.in);
+
+        //Input number
+        double number = scan.nextDouble();
+
+        if( ( number == X ) || ( number == Y ) || ( number == Z ) ) {
+            System.out.print("Данное значение имеется в константах");
+        }
+        else {
+            System.out.print("Такой константы нет!");
+        }
+    }
+}
